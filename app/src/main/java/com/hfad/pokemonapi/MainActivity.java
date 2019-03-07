c45449617c9859caf53285b896ee005edd61a91a
ianmclane15 <ian.mclane@me.com>
2019-03-07 09:34:08 -0800

    ywwt

--- app/src/main/java/com/hfad/pokemonapi/MainActivity.java
+++ app/src/main/java/com/hfad/pokemonapi/MainActivity.java
@@ -1,34 +1,91 @@
 package com.hfad.pokemonapi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.preference.PreferenceManager;
 import android.os.Bundle;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.android.gms.maps.MapView;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
 
-public class MainActivity extends AppCompatActivity {
 
-    private TextView latitude;
-    private TextView longitude;
+public class MainActivity extends Activity {
+
+    private TextView latitudeText;
+    private TextView longitudeText;
+    private Double latitude;
+    private Double longitude;
     private MapView mapView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    MapView map = null;
+    @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        GeoPoint geo = new GeoPoint(latitude, longitude);
+        geo.
+
+        Marker startMarker = new Marker(map);
+        startMarker.setPosition();
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        map.getOverlays().add(startMarker);
+
+        //handle permissions first, before map is created. not depicted here
+
+        //load/initialize the osmdroid configuration, this can be done
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        //setting this before the layout is inflated is a good idea
+        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
+        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
+        //see also StorageUtils
+        //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's tile servers will get you banned based on this string
+
+        //inflate and create the map
         setContentView(R.layout.activity_main);
 
-       wireWidgets();
-       getCoordinates();
+        map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        wireWidgets();
+        getCoordinates();
+
+
     }
 
+        public void onResume(){
+            super.onResume();
+            //this will refresh the osmdroid configuration on resuming.
+            //if you make changes to the configuration, use
+            //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+            //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+            map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+        }
+
+        public void onPause(){
+            super.onPause();
+            //this will refresh the osmdroid configuration on resuming.
+            //if you make changes to the configuration, use
+            //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+            //Configuration.getInstance().save(this, prefs);
+            map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+        }
+
     private void getCoordinates() {
+
+        latitudeText.setText("Latitude: " + latitudeText);
+        longitudeText.setText("Longitude: " + longitudeText);
 
     }
 
     private void wireWidgets() {
-        latitude = findViewById(R.id.textView_maps_latitude);
-        longitude = findViewById(R.id.textView_maps_longitude);
-        mapView = findViewById(R.id.mapView_map);
+        latitudeText = findViewById(R.id.textView_maps_latitude);
+        longitudeText = findViewById(R.id.textView_maps_longitude);
+        mapView = findViewById(R.id.map);
     }
-}
+    }
